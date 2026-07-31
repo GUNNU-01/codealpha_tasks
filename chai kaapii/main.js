a0643ab8a956bed85effffb9010e8f31b00c68e4
GUNNU-01 <gungunpanday61@gmail.com>
2026-08-01 00:13:23 +0530

    Add files via upload
    ecommerce website to a responsive food delivery app

---
+++ chai kaapii/main.js
@@ -0,0 +1,55 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+        nextEl: "#next",
+        prevEl: "#prev",
+    },
+});
+
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+const cardList = document.querySelector('.card - list');
+
+cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
+
+closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
+
+let productList = [];
+
+const showCards = () =>{
+
+    productList.forEach(product =>{
+
+    })
+
+const orderCard = document.createElement('div');
+
+        orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+          <div class="order-card">
+                            <img src="${product.image}">
+                        </div>
+                        <h4>${product.name}</h4>
+                        <h4 class="price">${product.price}</h4>
+                        <a href="#" class="btn">Add to Cart</a>
+                        ;
+
+                        cardList.appendChild(orderCard);
+                    
+        `
+
+const initApp = () =>{
+
+    fetch('product.json').then
+    (response => response.json()).then
+    (data =>{
+        productList = data;
+        showCards();
+    })
+}
+
+initApp();
+
